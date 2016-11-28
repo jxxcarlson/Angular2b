2e541b937d8fe3173f638eef6b624562fef9daa8
James Carlson <jxxcarlson@gmail.com>
2016-11-28 13:29:26 -0500

    Color the active document list item

--- src/app/document-list-item/document-list-item.component.ts
+++ src/app/document-list-item/document-list-item.component.ts
@@ -5,31 +5,11 @@ import { Document } from '../shared/document.model'
   selector: 'document-list-item',
   templateUrl: './document-list-item.component.html',
   styleUrls: ['./document-list-item.component.css'],
-  styles: [`
-    :host(.active) { 'background-color': '#ebb' }
-`, `
-li {
-
-  font-size: 1.25rem;
-  margin-bottom:0.35rem;
-  height:30px;
-  padding: 7px;
-  width: 300px;
-  color: black;
-
-}
-.active {
-  background-color: gray;
-  color: white;
-}
-
-`],
-
 })
 export class DocumentListItemComponent implements OnInit {
 
   @Input() document: Document
-  @Input() color: string
+  @Input() active: boolean
 
   constructor() { }
 
@@ -40,3 +20,30 @@ export class DocumentListItemComponent implements OnInit {
 
 
 }
+
+
+/***
+
+ styles: [`
+ :host(.active) { 'background-color': '#ebb' }
+ `, `
+ li {
+
+  font-size: 1.25rem;
+  margin-bottom:0.35rem;
+  height:30px;
+  padding: 7px;
+  width: 300px;
+  color: black;
+
+}
+ .active {
+  background-color: gray;
+  color: white;
+}
+
+ `],
+
+
+***/
+
